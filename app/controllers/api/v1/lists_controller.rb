1f94b9e5a7bbacc3e195b3fbb07d4482f4e28be5
blunce24 <blairlunceford@gmail.com>
2017-02-15 08:56:04 -0700

    Completed delete lists checkpoint

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -12,6 +12,16 @@ module Api::V1
       end
     end
 
+    def destroy
+      begin
+        list = List.find(params[:id])
+        list.destroy
+        render json: {}, status: :no_content
+      rescue ActiveRecord::RecordNotFound
+        render :json => {}, :status => :not_found
+      end
+    end
+
     private
     def list_params
       params.require(:list).permit(:title, :private)
